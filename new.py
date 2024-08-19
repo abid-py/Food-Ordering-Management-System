3f35ff978fbd44b7773d5a1beaae9206f567ccfd
MD ABIDUR RAHMAN ROHAN <ghoststory886@gmail.com>
2024-08-19 12:51:19 +0600

    Food Ordering Management System
    This all step will ahead by taking permission from customer.

---
+++ new.py
@@ -0,0 +1,27 @@
+c = input("Do you like to order FOOD (YES!/NO!)?").lower()
+total = 0
+if c == "yes":
+    print("______MENU______")
+    print(" ")
+    x = ["Pizza","Burger","Sandwitch"]
+    p = [360,150,40]
+    q = 1
+    for i in range(len(x)):
+        print(f"{q}. {x[i]}: ${p[i]}")
+        q += 1
+    print("________________________________________________")
+while c == "yes":
+    choice = input("Name of food item : ").capitalize()
+    quantity = int(input("Please select quantity : "))
+    if choice in x:
+        food = x.index(choice)
+        price = p[food]
+        j = (price*quantity)
+        print(f"Your order is {choice}: ${j}")
+        c = input("Do you like to order FOOD (YES!/NO!)?").lower()
+        total+= j
+        if c == "no":
+            print(f"Your price is {total}")        
+else:
+    print("Have A Nice Day!")
+
